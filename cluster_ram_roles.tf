67be0de7aa93661fcbe84a495110ea086fd7839d
umair-gujjar <engrumair_sabir@yahoo.com>
2021-10-26 14:13:50 +0200

    Update cluster_ram_roles.tf

--- cluster_ram_roles.tf
+++ cluster_ram_roles.tf
@@ -101,6 +101,31 @@ EOL
       additional_policy = null
     },
     {
+      role_name            = "AliyunCSManagedKubernetesRole"
+      description          = "Kubernetes role for ACK"
+      max_session_duration = 3600
+      role_document        = <<EOL
+{
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": [
+          "cs.aliyuncs.com"
+        ]
+      }
+    }
+  ],
+  "Version": "1"
+}
+EOL
+      policy_attachments = [
+        "AliyunCSManagedKubernetesRolePolicy"
+      ],
+      additional_policy = null
+    },
+    {
       role_name            = "AliyunESSRole"
       description          = "Used for auto-scaling"
       max_session_duration = 3600
